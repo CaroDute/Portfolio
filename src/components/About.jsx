a46350f779bc2cb4d35c1de957bd53e9bd396627
CaroDute <caro.duteurtre@orange.fr>
2024-06-24 19:22:43 +0200

    Ajout d'animation à l'arrivée sur le site

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,25 +10,27 @@ const About = () => {
           />
         </div>
         <div className="about__text">
+          <h1 className="about__text--title">Hey ! Je m'appelle Caroline, </h1>
           <p>
-            Je m'appelle Caroline et je suis developpeuse junior. Je
-            suis passionnée par le monde de la tech et particulierement du
-            developpement web dont j'ai choisi d'en faire mon métier suite à une
-            reconversion professionnelle. Je suis actuellement à la recherche
-            d'un emploi en tant que developpeuse front-end. J'ai réalisé de
-            nombreux projets durant ma formation dont vous pouvez avoir un
-            aperçu juste en dessous alors n'hésitez pas à aller découvrir ça et
-            si vous souhaitez me contacter le formulaire vous attend en bas de
-            page !
+            et je suis developpeuse junior. Je suis passionnée par le monde de
+            la tech et particulierement du developpement web dont j'ai choisi
+            d'en faire mon métier suite à une reconversion professionnelle. Je
+            suis actuellement à la recherche d'un emploi en tant que
+            developpeuse front-end. J'ai réalisé de nombreux projets durant ma
+            formation dont vous pouvez avoir un aperçu juste en dessous alors
+            n'hésitez pas à aller découvrir ça et si vous souhaitez me contacter
+            le formulaire vous attend en bas de page !
           </p>
         </div>
       </div>
       <div className="container">
-        <img
-          src="src/assets/images/Fleche-bleu.png"
-          alt="fleche bleu"
-          className="container__arrow"
-        />
+        <a href="#skills">
+          <img
+            src="src/assets/images/Fleche-bleu.png"
+            alt="fleche bleu"
+            className="container__arrow"
+          />
+        </a>
       </div>
     </>
   );
