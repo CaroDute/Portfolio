97939b0409e98ce0b1c0a5a0d4aff66d499af4e3
CaroDute <caro.duteurtre@orange.fr>
2024-07-02 16:15:14 +0200

    Responsive fait sur toute la page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/le_studio_de_caroline" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-instagram"></i></a>
+        <a href="https://www.facebook.com/caroline.duteurtre" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-facebook"></i></a>
+        <a href="https://x.com/Caro_Dute" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-twitter"></i></a>
+        <a href="https://github.com/CaroDute" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+      </div>
       <p>
       © Caroline Duteurtre | Tous droits réservés
       </p>
