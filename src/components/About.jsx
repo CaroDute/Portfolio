7838c92ad248c1e880ceb018fd14f7d59990fbd8
CaroDute <caro.duteurtre@orange.fr>
2024-07-03 18:43:18 +0200

    Mixins ajoutées

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import BureauImage from "/images/Bureau.webp";
 import FlecheJaune from "/images/fleche-jaune.webp";
+import Geometric from "/images/geometric.webp";
 
 const About = () => {
   return (
@@ -14,7 +15,12 @@ const About = () => {
           <p>
             je suis developpeuse junior. Je suis passionnée par le monde de la
             tech et particulierement du developpement web dont j'ai choisi d'en
-            faire mon métier suite à une reconversion professionnelle. Je suis
+            faire mon métier suite à une reconversion professionnelle. J'ai
+            suivi une formation d'intégration web auprès d'OpenClassrooms. j'ai
+            pu developper mes compétences en front-end, en travaillant
+            principalement sur les bases HTML/ CSS ainsi que Javascript, et pour
+            aller un peu plus loin, React et Redux. J'ai egalement pu developper
+            des compétences en SEO et en optimisation des performances. Je suis
             actuellement à la recherche d'un emploi en tant que developpeuse
             front-end. J'ai réalisé de nombreux projets durant ma formation dont
             vous pouvez avoir un aperçu juste en dessous alors n'hésitez pas à
